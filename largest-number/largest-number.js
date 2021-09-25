c7dcd64aabbe777bce18f40163c9e64d10816dec
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-09-24 22:36:07 -0700

    Time: 80 ms (73.08%), Space: 40.5 MB (49.74%) - LeetHub

--- largest-number/largest-number.js
+++ largest-number/largest-number.js
@@ -3,11 +3,20 @@
  * @return {string}
  */
 var largestNumber = function(nums) {
-    var res = nums.sort(function (a, b) {
-    var str1 = '' + a + b;
-    var str2 = '' + b + a;
-    if (str1 === str2) return 0;
-    return str1 > str2 ? -1 : 1;
-  }).join('');
-  return res[0] === '0' ? '0' : res;
+   var result = nums.sort(function(a, b) {
+       //create str1 for ab
+       var str1 = '' + a + b;
+       //create str2 for ba
+       var str2 = '' + b + a;
+       if(str1 === str2) {
+           return 0;
+       }
+       //if str1 is greater than str2 then sort in descending else dont sort 
+       return str1 > str2 ? -1 : 1;
+   })
+   if(result.join('')[0] === '0') {
+       return '0';
+   }else {
+       return result.join('');
+   }
 };
