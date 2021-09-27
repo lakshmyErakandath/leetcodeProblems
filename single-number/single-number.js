9cc583ff21d196939948c1b55724d3afc1cd4560
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-09-26 22:25:28 -0700

    Time: 1268 ms (5.02%), Space: 44.6 MB (19.92%) - LeetHub

---
+++ single-number/single-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let result = {}
+    for(var i = 0; i < nums.length; i++) {
+      var count = 0;
+        for(var j = 0; j < nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                count++
+               result[nums[i]] = count;
+            }
+        }
+    }
+    for(var vals in result) {
+      if(result[vals] === 1) {
+        return vals;
+      }
+    }
+};
