2638d504585904be64e287430ec5f99978d0d8aa
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-09-27 10:19:46 -0700

    Time: 72 ms (93.13%), Space: 41.9 MB (16.79%) - LeetHub

--- single-number-ii/single-number-ii.js
+++ single-number-ii/single-number-ii.js
@@ -4,14 +4,8 @@
  */
 var singleNumber = function(nums) {
      let result = {}
-    for(var i = 0; i < nums.length; i++) {
-      var count = 0;
-        for(var j = 0; j < nums.length; j++) {
-            if(nums[i] === nums[j]) {
-                count++
-               result[nums[i]] = count;
-            }
-        }
+    for(let num of nums) {
+        result[num] = result[num] +1 || 1
     }
     for(var vals in result) {
       if(result[vals] === 1) {
