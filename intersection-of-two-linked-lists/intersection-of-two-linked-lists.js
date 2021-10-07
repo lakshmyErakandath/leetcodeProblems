42540945a15b035932e15ec352bcd45b927680ae
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-10-06 23:32:27 -0700

    Time: 96 ms (71.74%), Space: 46 MB (36.55%) - LeetHub

---
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+// var getIntersectionNode = function(headA, headB) {
+//     let currA = headA;
+//     let currB = headB;
+//     let resA = headA;
+//     let resB = headB;
+//     let lenA=0;
+//     let lenB=0;
+//     while(currA.next){
+//         lenA++
+//         currA = currA.next;
+//     }
+//     while(currB.next) {
+//         lenB++;
+//         currB = currB.next;
+//     }
+//     let diff = Math.abs(lenA-lenB);
+//     if(lenA > lenB) {
+//         for(var i = 0; i < diff; i++) {
+//         resA = resA.next;
+//         }
+//     }else {
+//      for(var i = 0; i < diff; i++) {
+//         resB = resB.next;
+//     } 
+//     }
+   
+//     while(resA && resB) {
+//             // if(resA.val === resB.val) {
+//             //     if(resA.next===null && resB.next === null) {
+//             // return resA;
+//             // }
+//             // }
+//      if(resA.next === resB.next) {
+//             return resB.next
+//         }else {
+//             resA = resA.next;
+//             resB = resB.next;
+//         }
+//     }
+//     return null;
+    
+// };
+
+var getIntersectionNode = function(headA, headB) {
+  var lenA = getLen(headA);
+  var lenB = getLen(headB);
+  let diff = Math.abs(lenA - lenB);
+
+  if (lenA > lenB) {
+    while (diff--) headA = headA.next;
+  } else {
+    while (diff--) headB = headB.next;
+  }
+
+  while (headA !== headB) {
+    headA = headA.next;
+    headB = headB.next;
+  }
+
+  return headA;
+};
+
+var getLen = function (head) {
+  var len = 0;
+  while (head) {
+    len++;
+    head = head.next;
+  }
+  return len;
+};
