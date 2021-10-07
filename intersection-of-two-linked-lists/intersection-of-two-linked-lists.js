2f7a4467596639352031216e72d828f219e12b5e
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-10-07 10:45:02 -0700

    Time: 988 ms (6.41%), Space: 45.3 MB (93.64%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -54,30 +54,49 @@
     
 // };
 
+// var getIntersectionNode = function(headA, headB) {
+//   var lenA = getLen(headA);
+//   var lenB = getLen(headB);
+//   let diff = Math.abs(lenA - lenB);
+
+//   if (lenA > lenB) {
+//     while (diff--) headA = headA.next;
+//   } else {
+//     while (diff--) headB = headB.next;
+//   }
+
+//   while (headA !== headB) {
+//     headA = headA.next;
+//     headB = headB.next;
+//   }
+
+//   return headA;
+// };
+
+// var getLen = function (head) {
+//   var len = 0;
+//   while (head) {
+//     len++;
+//     head = head.next;
+//   }
+//   return len;
+// };
+
+
 var getIntersectionNode = function(headA, headB) {
-  var lenA = getLen(headA);
-  var lenB = getLen(headB);
-  let diff = Math.abs(lenA - lenB);
-
-  if (lenA > lenB) {
-    while (diff--) headA = headA.next;
-  } else {
-    while (diff--) headB = headB.next;
-  }
-
-  while (headA !== headB) {
-    headA = headA.next;
-    headB = headB.next;
-  }
-
-  return headA;
-};
-
-var getLen = function (head) {
-  var len = 0;
-  while (head) {
-    len++;
-    head = head.next;
-  }
-  return len;
+ 
+    while (headA) {
+        
+        let head2 = headB;
+        
+        while (head2) {
+            if(headA === head2){
+                return headA;
+            }
+            head2 = head2.next;
+        }
+        headA = headA.next;
+    }
+    
+    return null;
 };
