60473ad92fc4c05226b9cf1cc4d1920e9306dfdc
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-10-08 11:43:44 -0700

    Time: 544 ms (50.00%), Space: 77.9 MB (71.76%) - LeetHub

--- swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.js
+++ swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.js
@@ -10,24 +10,46 @@
  * @param {number} k
  * @return {ListNode}
  */
+// var swapNodes = function(head, k) {
+//     let result = [];
+//     let current = head;
+//     let resultHead = head;
+//     while(current) {
+//         result.push(current.val);
+//         current = current.next;
+//     }
+//     let temp = result[result.length-k];
+//     result[result.length-k] = result[k-1]
+//      result[k-1] = temp;
+    
+//     for(var i = 0; i < result.length; i++) {
+//         if(result[i] !== resultHead.val) {
+//             resultHead.val = result[i];
+//         }
+//         resultHead = resultHead.next;
+//     }
+//     return head;
+    
+// };
+
 var swapNodes = function(head, k) {
-    let result = [];
-    let current = head;
-    let resultHead = head;
-    while(current) {
-        result.push(current.val);
-        current = current.next;
+    let counter = 1;
+    let kfirst = head;
+  
+    while(counter !== k) {
+        counter++
+        kfirst = kfirst.next
     }
-    let temp = result[result.length-k];
-    result[result.length-k] = result[k-1]
-     result[k-1] = temp;
     
-    for(var i = 0; i < result.length; i++) {
-        if(result[i] !== resultHead.val) {
-            resultHead.val = result[i];
-        }
-        resultHead = resultHead.next;
+    let kfirstnode = kfirst;
+    let klast = head;
+    
+    while(kfirst.next) {
+        kfirst = kfirst.next;
+        klast = klast.next;
     }
+    let temp = klast.val;
+    klast.val = kfirstnode.val;
+    kfirstnode.val = temp;
     return head;
-    
-};
+}
