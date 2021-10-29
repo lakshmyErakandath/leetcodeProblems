867893ae0409b26450d16b63d6b5591a7b73bdbd
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-10-28 22:54:57 -0700

    Time: 72 ms (88.07%), Space: 40.2 MB (40.37%) - LeetHub

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -2,19 +2,31 @@
  * @param {string} sentence
  * @return {boolean}
  */
+
+//solution -1
+//-----------------
 var checkIfPangram = function(sentence) {
-    let letters = "abcdefghijklmnopqrstuvwxyz";
-    letters = letters.split('')
-    for(var i = 0; i < sentence.length; i++) {
-        if(letters.includes(sentence[i])) {
-            let index = letters.indexOf(sentence[i]);
-            letters.splice(index,1);
-        }
-    }
-    if(letters.length === 0) {
-        return true;
-    }else {
-            return false;
-    } 
+    sentence = sentence.split('');
+    let set = new Set(sentence);
     
-};
+    return set.size === 26;
+}
+
+// solution -2
+//---------------------
+// var checkIfPangram = function(sentence) {
+//     let letters = "abcdefghijklmnopqrstuvwxyz";
+//     letters = letters.split('')
+//     for(var i = 0; i < sentence.length; i++) {
+//         if(letters.includes(sentence[i])) {
+//             let index = letters.indexOf(sentence[i]);
+//             letters.splice(index,1);
+//         }
+//     }
+//     if(letters.length === 0) {
+//         return true;
+//     }else {
+//             return false;
+//     } 
+// };
+
